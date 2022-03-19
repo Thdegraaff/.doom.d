aa9c27f25321f06cfaf8e0652f6119eef4d8f358
Thomas de Graaff <t.de.graaff@vu.nl>
2022-03-19 13:18:34 +0100

    update config

--- config.el
+++ config.el
@@ -1,6 +1,10 @@
 (setq user-full-name "Thomas de Graaff (Tomba)"
       user-mail-address "t.de.graaff@vu.nl")
 
+(unless (equal "Battery status not available"
+               (battery))
+  (display-battery-mode 1))        ; On laptops it's nice to know how much power you have
+
 (setq inhibit-startup-message t)
 
 (server-start)
@@ -165,22 +169,46 @@
 
   (add-hook 'after-init-hook 'org-roam-mode)
 
-  ;; Let's set up some org-roam capture templates
-  (setq org-roam-capture-templates
-        (quote (("d" "default" plain (function org-roam--capture-get-point)
-                 "%?"
-                 :file-name "%<%Y-%m-%d-%H%M%S>-${slug}"
-                 :head "#+title: ${title}\n"
-                 :unnarrowed t)
-                )))
+    ;; org-roam-bibtex stuff
+    (use-package! org-roam-bibtex)
+    (org-roam-bibtex-mode)
 
-  ;; And now we set necessary variables for org-roam-dailies
-  (setq org-roam-dailies-capture-templates
-        '(("d" "default" entry
-           #'org-roam-capture--get-point
-           "* %?"
-           :file-name "daily/%<%Y-%m-%d>"
-           :head "#+title: %<%Y-%m-%d>\n\n"))))
+    (setq orb-preformat-keywords
+          '("citekey" "title" "url" "author-or-editor" "keywords" "file")
+          orb-process-file-keyword t
+          orb-file-field-extensions '("pdf"))
+
+    ;; Let's set up some org-roam capture templates
+    (setq org-roam-capture-templates
+          (quote (("d" "default" plain
+                   "%?"
+                   :target
+                   (file+head "%<%Y-%m-%d-%H%M%S>-${slug}.org"
+                                      "#+title: ${title}\n")
+                   :unnarrowed t)
+                  ("r" "bibliography reference" plain
+                   (file "~/org/org-roam/templates/orb-capture")
+                   :target
+                   (file+head "references/${citekey}.org" "#+title: ${title}\n")))))
+
+    ;; And now we set necessary variables for org-roam-dailies
+    (setq org-roam-dailies-directory "daily/")
+    (setq org-roam-dailies-capture-templates
+          '(("d" "default" entry
+             "* %?"
+             :target
+             (file+head "%<%Y-%m-%d>.org"
+                                "#+title: %<%Y-%m-%d>\n"))))
+
+    ;; Function to capture quotes from pdf
+    (defun org-roam-capture-pdf-active-region ()
+      (let* ((pdf-buf-name (plist-get org-capture-plist :original-buffer))
+             (pdf-buf (get-buffer pdf-buf-name)))
+        (if (buffer-live-p pdf-buf)
+            (with-current-buffer pdf-buf
+              (car (pdf-view-active-region-text)))
+          (user-error "Buffer %S not alive" pdf-buf-name))))
+)
 
  ;; org-roam-bibtex stuff
 (use-package org-roam-bibtex
